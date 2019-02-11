6fb19b024638607126c359b7261a513822bbf603
Grant Clark <gclark@cstl-01.cs.ucsb.edu>
2019-02-11 12:16:33 -0800

    lab05 completed

---
+++ lab05/table.cpp
@@ -0,0 +1,189 @@
+//  Grant Clark, 5224308
+
+#include "table.h"
+
+Table::Table(unsigned int max_entries) {
+    size = max_entries;
+    // Assigns the dynamically allocated array to be a new array of EntryVec (std::vector<Entry>)
+    entries = new EntryVec[size];
+}
+
+Table::Table(unsigned int entries, std::istream& input){
+    unsigned int currSize = 0;
+    size = entries;
+    this->entries = new EntryVec[size];
+    std::string inputString, keyString, dataString;
+    while(!input.eof() && currSize < size) {
+        Entry e;
+        input >> e;
+        put(e);
+        currSize++;
+    }
+}
+
+void Table::put(unsigned int key, std::string data){
+    Entry newEntry(key, data);
+    unsigned int hashedKey = hash(key);
+    entries[hashedKey].insert(std::lower_bound(entries[hashedKey].begin(), entries[hashedKey].end(), newEntry), newEntry);
+}
+
+void Table::put(Entry e){
+    unsigned int hashedKey = hash(e.get_key());
+    entries[hashedKey].insert(std::lower_bound(entries[hashedKey].begin(), entries[hashedKey].end(), e), e);
+}
+
+std::string Table::get(unsigned int key) const{
+    unsigned int hashedKey = hash(key);
+    EntryVec* e = &entries[hashedKey];
+    int pos = find(key, *e);
+    if(pos >= 0) {
+        return (e->begin() + pos)->get_data();
+    } else {
+        return "";
+    }
+}
+
+bool Table::remove(unsigned int key){
+    unsigned int hashedKey = hash(key);
+    EntryVec* e = &entries[hashedKey];
+    int pos = find(key, *e);
+    if(pos < 0) {
+        return false;
+    } else {
+        e->erase(e->begin() + pos);
+        return true;
+    }
+}
+
+int Table::find(unsigned int key, EntryVec e) const {
+    for(unsigned int i = 0; i < e.size(); i++) {
+        if(e[i].get_key() == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+unsigned int Table::hash(unsigned int key) const {
+    return (key%size);
+}
+
+std::ostream& operator<< (std::ostream& out, const Table& t){
+    // Vector storing objects of ExtEntry type. Will be a sorted vector (sorted by key) of the first elements of each
+    // of the vectors containing Entry objects in EntryVec in Table. If the first element is erased from the vector, it
+    // will try to add the next element from the original EntryVec that the erased element originated from.
+    ExtEntryVec outVector;
+
+    // Stores all first Entry objects into outVector (in sorted order)
+    for(unsigned int i = 0; i < t.size; i++) {
+        EntryVec e = t.entries[i];
+        if(!e.empty()) {
+            // Entry object, iterator to the place in the original vector for the Entry object, hashedKey/index
+            ExtEntry extE(e[0], 0, i);
+
+            ExtEntryVec::iterator extEntryIt = std::lower_bound(outVector.begin(), outVector.end(), extE);
+            outVector.insert(extEntryIt, extE);
+        }
+    }
+
+    // Loop continues until there are no more Entry objects in the original table that it hasn't printed out.
+    while(!outVector.empty()) {
+        // Gets the iterator to the original EntryVec for the object that will be erased from the temporary outVector.
+        unsigned int nextToInsert = outVector[0].get_origPos() + 1;
+        unsigned int hashedKey = outVector[0].get_hashedKey();
+        EntryVec origVec = t.entries[hashedKey];
+
+        // Writes the lowest key to the output.
+        Entry outEntry = outVector[0].get_entry();
+        out << outEntry << std::endl;
+
+        outVector.erase(outVector.begin());
+        EntryVec::iterator end = t.entries[hashedKey].end();
+
+        if(nextToInsert != origVec.size()) {
+            ExtEntry extE(origVec[nextToInsert], nextToInsert, hashedKey);
+
+            ExtEntryVec::iterator extEntryIt = std::lower_bound(outVector.begin(), outVector.end(), extE);
+            outVector.insert(extEntryIt, extE);
+        }
+    }
+
+    return out;
+}
+
+// Lab05
+Table::Table(const Table& t) {
+    size = t.size;
+    entries = new EntryVec[size];
+
+    for(unsigned int i = 0; i < size; i++) {
+        // Creates new vector from old vector (Deep Copy)
+        EntryVec newVec(t.entries[i]);
+        entries[i] = newVec;
+    }
+}
+
+Table::~Table() {
+    delete [] entries;
+}
+
+Table& Table::operator= (const Table& t) {
+    if(&t == this) {
+        return *this;
+    }
+
+    delete [] this->entries;
+
+    this->size = t.size;
+    this->entries = new EntryVec[size];
+
+    for(unsigned int i = 0; i < size; i++) {
+        EntryVec newVec(t.entries[i]);
+        entries[i] = newVec;
+    }
+
+    return *this;
+}
+
+
+
+
+
+
+
+
+ExtEntry::ExtEntry(Entry e, unsigned int oPos, unsigned int hKey) {
+    entry = e;
+    origPos = oPos;
+    hashedKey = hKey;
+}
+
+ExtEntry::ExtEntry(const ExtEntry &e) {
+    entry = e.get_entry();
+    origPos = e.get_origPos();
+    hashedKey = e.get_hashedKey();
+}
+
+Entry ExtEntry::get_entry() const {
+    return entry;
+}
+unsigned int ExtEntry::get_origPos() const {
+    return origPos;
+}
+unsigned int ExtEntry::get_hashedKey() const {
+    return hashedKey;
+}
+
+ExtEntry::operator unsigned int () const {
+    return get_entry().get_key();
+}
+
+ExtEntry& ExtEntry::operator = (const ExtEntry &other) {
+    if(&other == this) {
+        return *this;
+    }
+    this->entry = other.get_entry();
+    this->origPos = other.get_origPos();
+    this->hashedKey = other.get_hashedKey();
+    return *this;
+}
---
+++ lab05/table.h
@@ -0,0 +1,62 @@
+//  Grant Clark, 5224308
+//
+//  table.h
+//  W19 - Lab04
+//
+#ifndef table_h
+#define table_h
+
+#include "entry.h"
+#include <iostream>
+#include <vector>
+
+typedef std::vector<Entry> EntryVec;
+
+class Table {
+public:
+    // Member Functions
+    Table(unsigned int max_entries = 100);
+    Table(unsigned int entries, std::istream& input);
+    void put(unsigned int key, std::string data);
+    void put(Entry e);
+    std::string get(unsigned int key) const;
+    bool remove(unsigned int key);
+    int find(unsigned int key, EntryVec e) const;
+
+    // Non-Member Functions
+    friend std::ostream& operator<< (std::ostream& out, const Table& t);
+
+    // Lab05
+    Table(const Table& t);
+    ~Table();
+    Table& operator= (const Table& t);
+private:
+    unsigned int size = 0;
+    // Dynamically allocated array storing vector objects.
+    EntryVec* entries;
+
+    // Helper Functions
+    unsigned int hash(unsigned int key) const;
+};
+
+struct ExtEntry {
+public:
+    ExtEntry(Entry e, unsigned int oPos, unsigned int hashedKey);
+    ExtEntry(const ExtEntry &e);
+    Entry get_entry() const;
+    unsigned int get_origPos() const;
+    unsigned int get_hashedKey() const;
+    // operator conversion function simplifies comparisons
+    operator unsigned int () const;
+    ExtEntry& operator = (const ExtEntry& other);
+private:
+    Entry entry;
+    unsigned int origPos;
+    unsigned int hashedKey;
+};
+
+typedef std::vector<ExtEntry> ExtEntryVec;
+
+
+
+#endif /* table_h */
